126359f93e3121911c7c161c4720f6458695d9f2
AngelGSoto <gsoto.angel@gmail.com>
2021-06-13 05:40:28 -0300

    Working on plots for paper

--- programs/splus_lamost_spectra.py
+++ programs/splus_lamost_spectra.py
@@ -31,6 +31,12 @@ parser.add_argument("TableSplus", type=str,
                     default="teste-program",
                     help="Name of table, taken the prefix")
 
+parser.add_argument("--ymin", required=False, type=float, default=None,
+                    help="""Value y-axis min""")
+
+parser.add_argument("--ymax", required=False, type=float, default=None,
+                    help="""Value y-axis max""")
+
 cmd_args = parser.parse_args()
 file_spec = cmd_args.fileLamost + ".fits"
 file_table = cmd_args.TableSplus + ".ecsv"
@@ -123,12 +129,24 @@ for wll, magg, magerr in zip(wl_sp, mag, mag_err):
     err = np.sqrt(((c*10**(b*magg))**2)*(np.log(10)*b*magerr)**2)
     err_.append(err)
 
+# ymin and ymax
+#ymin = cmd_args.ymin
+#ymax = cmd_args.ymax
+
 # PLOTS
 fig, ax = plt.subplots(figsize=(12, 5))
 ax.spines["top"].set_visible(False)  
 ax.spines["right"].set_visible(False)
 ax.set(xlim=[3350,9300])
-#plt.ylim(ymin=-50.0,ymax=200)
+
+# set Y-axis range (if applicable)
+if cmd_args.ymin is not None and cmd_args.ymax is not None:
+    plt.ylim(cmd_args.ymin,cmd_args.ymax)
+elif cmd_args.ymin is not None:
+    plt.ylim(ymin=cmd_args.ymin)
+elif cmd_args.ymax is not None:
+    plt.ylim(ymax=cmd_args.ymax)
+ 
 ax.set(xlabel='Wavelength $(\AA)$')
 ax.set(ylabel='Normalized flux')
 ax.plot(wl, Flux, c = "gray", linewidth=1.3, alpha=0.6, zorder=5)
@@ -137,7 +155,18 @@ for wl1, mag, magErr, colors, marker_ in zip(wl_sp, mag, err_, color, marker): #
     F *= factor
     ax.scatter(wl1, F, c = colors, marker=marker_, s=80, zorder=4)
     ax.errorbar(wl1, F, yerr=magErr, marker='.', fmt='.', color=colors, ecolor=colors, elinewidth=3.9, markeredgewidth=3.2, capsize=10)
-ax.axvline(4686, color='r', linewidth=0.3, linestyle='-', zorder = 6, label="He II")
+#ax.axvline(4686, color='r', linewidth=0.3, linestyle='-', zorder = 6, label="He II")
+
+if cmd_args.ymax is not None:
+    ax.annotate(str(table["ID"][ind]).split("R3.")[-1].replace(".", "-"), xy=(9000, cmd_args.ymax),  xycoords='data', size=13,
+            xytext=(-120, -60), textcoords='offset points', 
+            bbox=dict(boxstyle="round4,pad=.5", fc="0.94"),)
+    ax.annotate("r=" + format(float(table["R_PStotal"][ind]), '.2f'), xy=(9000, 0.85*cmd_args.ymax),  xycoords='data', size=13,
+            xytext=(-120, -60), textcoords='offset points', 
+            bbox=dict(boxstyle="round4,pad=.5", fc="0.94"),)
+else:
+    None
+
 ax.legend()
 plt.tight_layout()
 asciifile = file_spec.replace(".fits", 
